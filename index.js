038710c5ad1b846f25e9c59cf923170e3d82351b
Philip osir Otieno <philiposir2@gmail.com>
2024-06-29 04:50:20 +0300

    add scopes assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var  customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+  return customerName;
+}
+
+
+function setBestCustomer(){
+bestCustomer = 'not bob'
+return bestCustomer
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'
+  return bestCustomer
+}
+
+const leastFavoriteCustomer = 'erick';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'andrew';
+  return leastFavoriteCustomer;
+}
+// console.log(changeLeastFavoriteCustomer());
